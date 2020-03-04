2f9c5d93ced105d301c5350dd611ce6f90413e94
Nobralyra <signees96@gmail.com>
2020-03-04 16:29:58 +0100

    Misses comments

--- Client.py
+++ Client.py
@@ -1,4 +1,6 @@
+import re
 import socket
+import sys
 
 # Create a UDP socket
 sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -6,53 +8,77 @@ sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 udp_IP = "127.0.0.1"
 udp_PORT = 9090
 server_address = (udp_IP, udp_PORT)
-message = ""
 bytes_to_be_read = 4096
 # how many messages has been send
 seg_number = 0
 
 
-try:
-
-    # Send data
-    print('sending {!r}'.format(message))
-    # uses sendto() to send messages to the server’s address.
-    # sent = sock.sendto(message, server_address)
-
-    # Receive response
-    print('waiting to receive')
-    # data, server = sock.recvfrom(bytes_to_be_read)
-    # print('received {!r}'.format(data))
-
-finally:
-    print('closing socket')
-    #sock.close()
-
-
 def send_message():
+    global seg_number
     print("Write a message")
     message = input()
-    message_to_server = "msg-" + str(seg_number) + "=" + message
-    sock.sendto(message_to_server, server_address)
+    message_to_server = "msg-" + str(seg_number) + " = " + message
+    sock.sendto(message_to_server.encode(), server_address)
+    print("C: " + message_to_server)
+    receive_message()
 
 
 def receive_message():
-    receive_message_server = sock.recvfrom(bytes_to_be_read)
-    return receive_message_server
+    try:
+        global seg_number
+        while True:
+            receive_message_from_server, server_address = sock.recvfrom(bytes_to_be_read)
+            print("S: " + receive_message_from_server.decode())
+
+            do_res_match = re.findall(r'res', receive_message_from_server.decode())
+            do_res_match_join = ' '.join(do_res_match)
+
+            do_seg_number_match = re.findall(r'\d+', receive_message_from_server.decode())
+            do_seg_number_match_join = ' '.join(do_seg_number_match)
+
+            if do_res_match_join.__eq__("res") & do_seg_number_match_join.__eq__(str(seg_number + 1)):
+                seg_number += 2
+                send_message()
+
+            else:
+                error_with_message_server = "Invalid message. Closing the client"
+                print("C: " + error_with_message_server)
+                sock.sendto(error_with_message_server.encode(), server_address)
+                return
+
+    finally:
+        sock.close()
+        sys.exit(1)
 
 
 def handshake():
-    while True:
+    try:
         syn = "com-" + str(seg_number) + " " + udp_IP
         print("C: " + syn)
-        syn_to_server = str.encode(syn)
-        sock.sendto(syn_to_server, server_address)
-        #print(syn.str.encode)
+        sock.sendto(syn.encode(), server_address)
 
-        receive = receive_message()
-        if receive:
-            message_from_server = receive[0].decode().split(" ", 1)
+        receive, address = sock.recvfrom(bytes_to_be_read)
+        print("S: " + receive.decode())
+        if receive.decode() == "com-0 accept " + udp_IP:
+            ack = "com-" + str(seg_number) + " accept"
+            print("C: " + ack)
+            sock.sendto(ack.encode(), server_address)
+            send_message()
+
+        else:
+            error_with_handshake_server = "Invalid syn_ack. Closing the client"
+            print("S: " + error_with_handshake_server)
+            sock.sendto(error_with_handshake_server.encode(), address)
+            return
+
+    except socket.timeout:
+        raise RuntimeError("No activity from server")
+
+    finally:
+        sock.close()
+        sys.exit(1)
 
 
 if handshake():
     send_message()
+    receive_message()
--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
+import re
 import socket
-
+import sys
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,45 +12,101 @@ sock.bind(server_address)
 bytes_to_be_read = 4096
 # how many messages has been send
 seg_number = 0
+sock.settimeout(10000)
 
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+"""
+Function that sends an answer to the client, if the server receive a message from the client.
+We need to add one to seg_number because of the protocol and have the address of the client as an parameter"""
 
-def receive_syn_message():
 
-    receive_message_server = sock.recvfrom(bytes_to_be_read)
-    return receive_message_server
+def send_message(address):
+    global seg_number
+    seg_number += 1
+    server_says = "res-" + str(seg_number) + " = " + "I am server"
+    sock.sendto(server_says.encode(), address)
+    print("S: " + server_says)
+
+
+"""Function that receives a message from the client and have address as an parameter because we send a message to the 
+client if there was an timeout """
 
 
 def receive_message():
-    while True:
-        receive = receive_syn_message()
-        if receive:
-            message_from_client = receive[0].decode().split(" ", 1)
+    """Try is for the exception that can happen and a finally that close the socket and give an exit code 1 that
+    means something wrong happend """
+    try:
+        global seg_number
+        first_message = True
+        while True:
+            receive_message_from_client, address = sock.recvfrom(bytes_to_be_read)
+            print("C: " + receive_message_from_client.decode())
+
+            do_msg_match = re.findall(r'msg', receive_message_from_client.decode())
+            do_msg_match_join = ' '.join(do_msg_match)
+            do_seg_number_match = re.findall(r'\d+', receive_message_from_client.decode())
+            do_seg_number_match_join = ' '.join(do_seg_number_match)
+
+            if do_msg_match_join.__eq__("msg") and do_seg_number_match_join.__eq__(str(seg_number)) and first_message:
+                first_message = False
+                send_message(address)
+
+            elif do_msg_match_join.__eq__("msg") & do_seg_number_match_join.__eq__(str(seg_number + 1)) and not first_message:
+                seg_number += 1
+                send_message(address)
+
+            else:
+                error_with_message_client = "msg protocol has an error. Closing the server"
+                print("S: " + error_with_message_client)
+                sock.sendto(error_with_message_client.encode(), address)
+                sock.gettimeout()
+                return
+
+    except socket.timeout:
+        timeout_server = "No activity from client"
+        print(timeout_server)
 
 
+    except OSError as error:
+        print("OS error: {0}".format(error))
+
+    finally:
+        sock.close()
+        sys.exit(1)
 
 
 def handshake():
-    while True:
-        receive = receive_syn_message()
+    try:
+        receive_syn, address = sock.recvfrom(bytes_to_be_read)
+        print("C: " + receive_syn.decode())
+        if receive_syn.decode() == "com-0 " + server_IP:
+            syn_ack = "com-" + str(seg_number) + " accept " + server_IP
+            print("S: " + syn_ack)
+            sock.sendto(syn_ack.encode(), address)
 
-        if receive:
-            message_from_client = receive[0].decode().split(" ", 1)
+            receive_ack, address = sock.recvfrom(bytes_to_be_read)
+            print("C: " + receive_ack.decode())
+            if receive_ack.decode() == "com-0 accept":
+                receive_message()
+
+            else:
+                return
+        else:
+            error_with_handshake_client = "Invalid syn or ack request. Closing the server"
+            print("S: " + error_with_handshake_client)
+            sock.sendto(error_with_handshake_client.encode(), address)
+            sock.gettimeout()
+
+    # If there is no reply from the server or the handshake did not go well
+    except socket.timeout:
+        print("No activity from client in handshake")
+
+    except OSError as error:
+        print("OS error: {0}".format(error))
+
+    finally:
+        sock.close()
+        sys.exit(1)
 
 
-    # When a message arrives, we proceed to read it with recvfrom(4096) , where 4096 is the number of bytes to be
-    # read, and unpack the return value in to data and address
-    data, address = sock.recvfrom(bytes_to_be_read)
-
-    print('received {} bytes from {}'.format(len(data), address))
-    print(data)
-
-    if data:
-        serverSays = b"I am server"
-        sent = sock.sendto(serverSays, address)
-        print('sent {} bytes back to {}'.format(sent, address))
-
-
-if(handshake()):
+if handshake():
     receive_message()
